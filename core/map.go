89eec2c42d0ab27fd8ddd4db91c67d0b5a55e865
Wordluc <frangiamoreluca@gmail.com>
2026-08-04 21:58:47 +0200

    feat: definition of block

--- core/map.go
+++ core/map.go
@@ -6,18 +6,18 @@ import (
 )
 
 type Map struct {
-	cells []ICell
+	cells []*BaseCell
 	size  common.Vec[int32]
 }
 
 func NewMap(size common.Vec[int32]) Map {
 	return Map{
-		cells: make([]ICell, size.X*size.Y),
+		cells: make([]*BaseCell, size.X*size.Y),
 		size:  size,
 	}
 }
 
-func (m *Map) FeedMap(newCell func(x, y int32) ICell) {
+func (m *Map) FeedMap(newCell func(x, y int32) *BaseCell) {
 	var i int32
 	for range m.cells {
 		m.cells[i] = newCell(i%m.size.X, i/m.size.X)
@@ -25,30 +25,48 @@ func (m *Map) FeedMap(newCell func(x, y int32) ICell) {
 	}
 }
 
-func (m *Map) SetRawCell(c ICell, pos common.Vec[int32]) error {
+func (m *Map) SetRawCell(c *BaseCell, pos common.Vec[int32]) error {
 	p := pos.X + (pos.Y * m.size.X)
 	m.cells[p] = c
 	return nil
 }
 
-func (m *Map) SetCells(newCell func(x, y int32) ICell, pos, size common.Vec[int32]) error {
+func (m *Map) GetNeighborhoodCells(pos common.Vec[int32], size common.Vec[int32]) (res map[common.Vec[int32]]*BaseCell, err error) {
+	if size.X <= 0 || size.Y <= 0 {
+		return nil, errors.New("invalid neighborhood size")
+	}
+	if size.X%2 == 0 || size.Y%2 == 0 {
+		return nil, errors.New("neighborhood size must be odd")
+	}
+	halfX := size.X / 2
+	halfY := size.Y / 2
+
+	res = make(map[common.Vec[int32]]*BaseCell, size.X*size.Y)
 	for iy := range size.Y {
 		for ix := range size.X {
-			p := (pos.X + (ix - size.X/2) + (pos.Y+(iy-size.Y/2))*m.size.X)
-			m.cells[p] = newCell(ix, iy)
+			worldX := pos.X + (ix - halfX)
+			worldY := pos.Y + (iy - halfY)
+
+			if worldX < 0 || worldY < 0 || worldX >= m.size.X || worldY >= m.size.Y {
+				continue
+			}
+
+			idx := worldX + worldY*m.size.X
+			pos := common.Vec[int32]{X: worldX, Y: worldY}
+			res[pos] = m.cells[idx]
 		}
 	}
-	return nil
+	return res, nil
 }
 
-func (m *Map) GetCell(pos common.Vec[int32]) (res ICell, err error) {
-	if pos.X < 0 || pos.Y < 0 {
-		return nil, errors.New("Error getting cell")
+func (m *Map) GetCell(pos common.Vec[int32]) (res *BaseCell, err error) {
+	if pos.X < 0 || pos.Y < 0 || pos.X >= m.size.X || pos.Y >= m.size.Y {
+		return nil, errors.New("Out of bound")
 	}
 	return m.cells[pos.X+pos.Y*m.size.X], nil
 }
 
-func (m *Map) ForEach(f func(x, y int32, cell ICell) error) (err error) {
+func (m *Map) ForEach(f func(x, y int32, cell *BaseCell) error) (err error) {
 	for iy := range m.size.Y {
 		for ix := range m.size.X {
 			err = f(ix, iy, m.cells[ix+iy*m.size.X])
--- core/world.go
+++ core/world.go
@@ -1,7 +1,70 @@
 package core
 
+import (
+	"GameOfLife/common"
+	"errors"
+	"fmt"
+	"slices"
+)
+
 var TOUCH_ID int
 
 type World struct {
-	Map Map
+	Map       Map
+	cellBlock map[CellType][]*BaseCell
+}
+
+func NewWorld(size common.Vec[int32]) (w World) {
+	w.Map = NewMap(size)
+	w.cellBlock = map[CellType][]*BaseCell{}
+	return w
+}
+
+func (w *World) GenerateMap() {
+	w.Map.FeedMap(func(x, y int32) *BaseCell {
+		return new(NewGrassCell())
+	})
+}
+
+func (w *World) GetCellBlock(cellType CellType) ([]*BaseCell, error) {
+	return slices.Clone(w.cellBlock[cellType]), nil
+}
+
+func (w *World) AddBlock(cellType CellType, pos common.Vec[int32], size common.Vec[int32]) error {
+	halfX := size.X / 2
+	halfY := size.Y / 2
+	if size.X <= 0 || size.Y <= 0 {
+		return errors.New("invalid neighborhood size")
+	}
+	if size.X%2 == 0 || size.Y%2 == 0 {
+		return errors.New("neighborhood size must be odd")
+	}
+	if pos.X-halfX < 0 || pos.Y-halfY < 0 || pos.X+halfX >= w.Map.size.X || pos.Y+halfY >= w.Map.size.Y {
+		return errors.New("Out of bound")
+	}
+
+	neighborhood, err := w.Map.GetNeighborhoodCells(pos, size)
+	if err != nil {
+		return err
+	}
+	definition, err := GetCellDefinition(cellType)
+	if err != nil {
+		return err
+	}
+	if definition.WhereCan != nil {
+		for _, n := range neighborhood {
+			if !slices.Contains(definition.WhereCan, n.GetType()) {
+				return fmt.Errorf("House not support in %v", neighborhood[pos].GetType())
+			}
+		}
+	}
+	for p := range neighborhood {
+		cell := new(definition.Construtor())
+		err = w.Map.SetRawCell(cell, p)
+		if err != nil {
+			return err
+		}
+		w.cellBlock[cellType] = append(w.cellBlock[cellType], cell)
+	}
+	return nil
 }
